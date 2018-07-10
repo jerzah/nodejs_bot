54026b3870a78a4740dbdd6868957a33155b3cd5
erode <unusualgallery@gmail.com>
2018-07-10 20:46:51 +0200

    commit 2

--- firstbot.js
+++ firstbot.js
@@ -1,7 +1,14 @@
 var irc = require('irc')
-var client = new irc.Client('irc.freenode.net', 'TestNick', {
-    channels: ['#testchannel']
+var client = new irc.Client('irc.undernet.net', 'BruckerNet', {
+    channels: ['#bruckernet']
 });
+
+function ircAuth(nick, key) {
+        this.nick = nick,
+        this.key = key,
+        this.authorised = false
+};
+    
 
 client.on('error', function(message) {
     console.error('error: ', message);
@@ -15,6 +22,13 @@ client.on('message', function (from, to, message) {
     console.log(from + ' --> ' + to + ': ' +  message);
     if (message == "Hello"){
         client.say(to, "Hi " + from);
-        console.log("Replised HI --> " + from)
+        console.log("'Replised: HI'" + from +"' --> " + from)
+    }
+    if ("Eroade" == from){
+        console.log("Owned");
+        var test = new ircAuth(from, "testkeywooo");
+        test.authorised = true;
+        console.log(test.authorised);
+         
     }
 });
