0cde72c3667e989b8913f3fcc54dfef32ae96f8f
erode <unusualgallery@gmail.com>
2018-07-11 17:13:47 +0200

    added join information, including: - date/time - nick - channel - message ;

--- firstbot.js
+++ firstbot.js
@@ -1,6 +1,11 @@
 var irc = require('irc')
 var client = new irc.Client('irc.undernet.org', 'BruckerNet', {
-    channels: ['#bruckernet']
+    channels: ['#bruckernet'],
+   /userName: '~aLameBotbla',
+    realName: 'myTestBot',
+    floodProtection: true,
+    floodProtectionDelay: 1000,
+    autoRejoin: true,
 });
 
 function ircAuth(nick, key) {
@@ -9,7 +14,6 @@ function ircAuth(nick, key) {
         this.authorised = false
 };
     
-
 client.on('error', function(message) {
     console.error('error: ', message);
 });
@@ -18,18 +22,28 @@ client.on('connect', function() {
     console.log('connected to irec server');
 });
 
+client.on('join', function(channel, nick, message) {
+    var date =new Date();
+    console.log(date);
+    console.log(nick + " joined " + channel);
+    console.log(message);
+});
+
 client.on('message', function (from, to, message) {
-    console.log(from + ' --> ' + to + ': ' +  message);
     if (message == "Hello"){
         client.say(to, "Hi " + from);
-        console.log("'Replised: HI'" + from +"' --> " + from)
+        console.log("'Replised: HI'" + from +"' --> " + from);
     }
-    if ("Eroade" == from && msg == "testkeywooo"){
+    if ("Eroade" == from && message == "testkeywooo") {
         console.log("Owned");
         var test = new ircAuth(from, "testkeywooo");
+        client.on('message#bruckernet', function (nick, text, message) {
+            console.log(nick, text, message);
+        });
         test.authorised = true;
         console.log(test.authorised);
-        client.say(from, "Authorised") 
-        console.log(from, "Authorised") 
-    }
+        client.say(from, "Authorised"); 
+        console.log(from, "Authorised"); 
+    } 
+    console.log(from + " say's " + "'" + message + "'" + " to: " +  to);
 });
